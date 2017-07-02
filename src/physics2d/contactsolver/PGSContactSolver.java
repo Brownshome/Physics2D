35e10f1178b12f12eec3a3db1a89a110d441d5ad
James Brown <jamesphone180@gmail.com>
2017-07-02 13:41:13 +1200

    Avoided divide by zero

--- src/physics2d/contactsolver/PGSContactSolver.java
+++ src/physics2d/contactsolver/PGSContactSolver.java
@@ -18,10 +18,14 @@ public class PGSContactSolver implements ContactSolver {
 	
 	@Override
 	public void solveContactPoints(Collection<ContactPoint> contactPoints) {
+		if(contactPoints.size() == 0)
+			return;
+		
 		/* Iterate though the list on contact points until an accuracy requirement is met or
 		 * the maximum number of iterations is taken.
 		 * 
 		 * We may need to partition the contact points set into unrelated sets if needed.
+		 * Also applying impulses to groups of contacted objects may help to alleviate mass differences
 		 */
 		
 		for(int i = 0; i < maximumIterationCount; i++) {
@@ -38,16 +42,16 @@ public class PGSContactSolver implements ContactSolver {
 
 	/** Returns true if the collision constraint has been met */
 	private boolean solveSingleIteration(Collection<ContactPoint> contactPoints) {
-		double accuracy = 0;
+		double convergance = 0;
 		
 		for(ContactPoint point : contactPoints) {
 			point.solveImpulse();
 		}
 		
 		for(ContactPoint point : contactPoints) {
-			accuracy += point.accuracy();
+			convergance += point.convergance();
 		}
 		
-		return accuracy / contactPoints.size() >= accuracyTarget;
+		return convergance / contactPoints.size() >= accuracyTarget;
 	}
 }
