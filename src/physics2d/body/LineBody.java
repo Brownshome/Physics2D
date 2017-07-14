68de5357bdb38b0d5e288757b652ef45b35b1ae8
RodgerRG <RodgerG@hotmail.co.nz>
2017-07-15 10:14:40 +1200

    Refactoring for new BroadShape

--- src/physics2d/body/LineBody.java
+++ src/physics2d/body/LineBody.java
@@ -8,20 +8,12 @@ import physics2d.maths.MutableVec2;
 import physics2d.maths.Rotation;
 
 public class LineBody extends DynamicBody{
-
-	public LineBody(MutableVec2 position, Rotation direction, MutableVec2 velocity, double angularVelocity,
-			BroadShape broadShape, NarrowShape narrowShape, double mass, double inertia, double restitution) {
-		super(position, direction, velocity, angularVelocity, broadShape, narrowShape, mass, inertia, restitution);
-	}
 	
 	public LineBody(MutableVec2 position, MutableVec2 velocity, MutableVec2 direction, double length) {
 		super(position, new MutableRotation(), velocity, 0, 1, 100, 1.0);
 		MutableVec2 realPos = new MutableVec2(position);
 		realPos.add(-Math.abs(direction.x()) * length, -Math.abs(direction.y()) * length);
 		
-		BroadShape broadShape = new BroadShape(realPos, Math.abs(direction.y()) * length * 2, Math.abs(direction.x()) * length * 2);
-		LineShape lineShape = new LineShape(position, length, direction);
-		setCollisionShapes(broadShape, lineShape);
 	}
 	
 }
